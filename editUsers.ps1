393567451c6a3ca75933c9acc17ccc6e83d02218
TommyRiquet <tommy.riquet@hotmail.com>
2023-01-07 12:12:30 +0100

    feat(Users):Modifcation des comptes
    Script permettant la modification de comptes utilisateurs dans l'AD

---
+++ editUsers.ps1
@@ -0,0 +1,27 @@
+# Import du module ActiveDirectory 
+# https://learn.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2022-ps
+Import-Module ActiveDirectory
+
+$CSVFile = "C:\Scripts\MODIFY_AD_USERS\Utilisateurs.csv"
+
+$users = Import-Csv -Path $CSVFile -Header "Username", "NewFirstName", "NewLastName", "NewUsername"
+
+Foreach($user in $users) {
+    $Username= $user.Username
+    $NewFirstName= $user.NewFirstName
+    $NewLastName= $user.NewLastName
+    $NewUsername= $user.NewUsername
+
+    if (Get-ADUser -Filter {SamAccountName -eq $Username}) {
+        try {
+            Set-ADUser -Identity (Get-ADUser -Filter {SamAccountName -eq $Username}) -GivenName $NewFirstName -Surname $NewLastName -SamAccountName $NewUsername
+            Write-Host "$Username a bien ete modifie"
+        }
+        catch {
+            Write-Host "Une erreur s'est produite lors de la modification de $Username"
+        }
+    }
+    else {
+        Write-Host "Il n'y a pas d'utilisateur $Username"
+    }
+}
