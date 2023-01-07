53baf66dcc75ed87d6cc02d1c9e766c3e584f1ed
TommyRiquet <tommy.riquet@hotmail.com>
2023-01-07 12:09:36 +0100

    feat(Users):Désactivation des comptes utilisateurs
    Script permettant la désactivation automatique des comptes utilisateurs dans l'AD

---
+++ disableUsers.ps1
@@ -0,0 +1,23 @@
+# Import du module ActiveDirectory 
+# https://learn.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2022-ps
+Import-Module ActiveDirectory
+
+$CSVFile = "C:\Scripts\DISABLE_AD_USERS\Utilisateurs.csv"
+$CSVData = Import-CSV -Path $CSVFile -Delimiter ";" -Encoding UTF8
+
+Foreach($Utilisateur in $CSVData){
+    $UtilisateurUsername = $Utilisateur.Username
+
+   # On vérifie l'existence d'un utilisateur 
+   if (Get-ADUser -Filter {SamAccountName -eq $UtilisateurUsername} -SearchBase "OU=utilisateurs,DC=l2-2,DC=lab")
+   {
+       # Si l'utilisateur existe
+       Disable-ADAccount $UtilisateurUsername
+       Write-Output "L'utilisateur $UtilisateurUsername a été désactivé"
+   }
+   else
+   {
+        # Si l'utilisateur n'existe pas
+        Write-Warning "L'utilisateur $UtilisateurUsername n'existe pas"
+   }
+}
